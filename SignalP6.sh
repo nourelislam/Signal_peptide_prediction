998d99d9a448bc175986b02d89443799eacbf32c
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2022-01-05 11:57:56 +0200

    Create SignalP6.sh

---
+++ SignalP6.sh
@@ -0,0 +1,5 @@
+#Running the Signalp6 for BSF###
+signalp6 -ff uniprot-compressed_true_download_true_format_fasta_query_proteome_3A-2022.01.03-07.35.10.32.fasta -od out_BSF/ -org eukarya -fmt all
+-fmt for formating
+-org for organism
+-ff for Fasta file
